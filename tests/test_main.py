e2e957efe4a039b9a4d49c2009a7210850942e63
Omar Crosby <omar.crosby@gmail.com>
2024-12-11 01:08:50 -0500

    chore: wip

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,19 +1,15 @@
-from fastapi.testclient import TestClient
-from app.main import app
 
-client = TestClient(app)
-
-def test_read_root():
+def test_read_root(client):
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"Hello": "World"}
 
-def test_health_check():
+def test_health_check(client):
     response = client.get("/healthz")
     assert response.status_code == 200
     assert response.json() == {"status": "ok"}
 
-def test_readiness_check():
+def test_readiness_check(client):
     response = client.get("/readiness")
     assert response.status_code == 200
     assert response.json() == {"status": "ready"}
